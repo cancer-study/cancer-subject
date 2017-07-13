c1f20a7b43b39a71bbf8ba5d937271d6edf8c903
aseokana <aseokana@bhp.org.bw>
2017-07-13 16:56:04 +0200

    modifying model

--- cancer_subject/models/treatment_response.py
+++ cancer_subject/models/treatment_response.py
@@ -3,19 +3,22 @@ from django.db import models
 from ...cancer_list.models import InfoDeterminant
 
 from ..choices.treatment_response import CANCER_RESPONSE
-from cancer_subject.models.model_mixins.crf_model_mixin import CrfModelMixin
+from .model_mixins import CrfModelMixin
 
 
 class TreatmentResponse (CrfModelMixin):
 
     tx_response_class = models.CharField(
-        verbose_name="1. Response to cancer treatment as classified by oncologist / doctor?",
+        verbose_name="1. Response to cancer treatment as classified "
+        "by oncologist / doctor?",
         max_length=95,
         choices=CANCER_RESPONSE,
         help_text="",)
 
-    tx_info_determinant = models.ManyToManyField(InfoDeterminant,
-        verbose_name="2. Information used by oncologist / doctor to determine treatment response?",
+    tx_info_determinant = models.ManyToManyField(
+        InfoDeterminant,
+        verbose_name="2. Information used by oncologist / doctor "
+        "to determine treatment response?",
         max_length=45,
         help_text="",)
 
@@ -24,7 +27,8 @@ class TreatmentResponse (CrfModelMixin):
         max_length=25,)
 
     tx_response = models.TextField(
-        verbose_name=("4. Briefly describe response to treatment and information used to judge "
+        verbose_name=("4. Briefly describe response to treatment and "
+                      "information used to judge "
                       "treatment response: "),
         max_length=350,
         help_text="",)
