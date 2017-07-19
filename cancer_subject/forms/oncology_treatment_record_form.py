30ee35c344146e4628af503903387519ce113224
aseokana <aseokana@bhp.org.bw>
2017-07-19 11:54:22 +0200

    linking validations to the forms

--- cancer_subject/forms/oncology_treatment_record_form.py
+++ cancer_subject/forms/oncology_treatment_record_form.py
@@ -1,8 +1,13 @@
-from django import forms
+from cancer_subject_validations.form_validators import (
+    OncologyTreatmentRecordChemoFormValidation,
+    OncologyTreatmentRecordRadiationFormValidation)
 
 from .modelform_mixin import SubjectModelFormMixin
-from ..models import OncologyTreatmentRecord, OTRChemo, OTRRadiation, OTRSurgical
-from edc_constants.constants import YES, NO
+from ..models import (
+    OncologyTreatmentRecord,
+    OTRChemo,
+    OTRRadiation,
+    OTRSurgical)
 
 
 class OncologyTreatmentRecordForm (SubjectModelFormMixin):
@@ -15,16 +20,9 @@ class OncologyTreatmentRecordForm (SubjectModelFormMixin):
 class OTRChemoForm (SubjectModelFormMixin):
 
     def clean(self):
-        cleaned_data = self.cleaned_data
-        if cleaned_data['chemo_delays'] == YES and not cleaned_data['why_delayed']:
-            raise forms.ValidationError('If doses have been delayed, what is the reason for the delay')
-        if cleaned_data['chemo_delays'] == NO and cleaned_data['why_delayed']:
-            raise forms.ValidationError('NO doses where delayed, do not provide any delay reasons')
-        if cleaned_data['chemo_reduced'] == YES and not cleaned_data['why_reduced']:
-            raise forms.ValidationError('If doses have been reduced, why were the doses reduced')
-        if cleaned_data['chemo_reduced'] == NO and cleaned_data['why_reduced']:
-            raise forms.ValidationError('NO doses reduced, do not provide any reasons for reducing dose')
-        cleaned_data = super(OTRChemoForm, self).clean()
+        cleaned_data = super().clean()
+        cleaned_data = OncologyTreatmentRecordChemoFormValidation(
+            cleaned_data=cleaned_data).clean()
         return cleaned_data
 
     class Meta:
@@ -33,13 +31,11 @@ class OTRChemoForm (SubjectModelFormMixin):
 
 
 class OTRRadiationForm (SubjectModelFormMixin):
-    def clean(self):
-        cleaned_data = self.cleaned_data
 
-    # validating radiation
-        if cleaned_data['radiation_details'] == 'Yes' and not cleaned_data['concomitant'] and not cleaned_data['amount_radiation']:
-            raise forms.ValidationError('If patient radiation details are available, please provide remaining details on form')
-        cleaned_data = super(OTRRadiationForm, self).clean()
+    def clean(self):
+        cleaned_data = super().clean()
+        cleaned_data = OncologyTreatmentRecordRadiationFormValidation(
+            cleaned_data=cleaned_data).clean()
         return cleaned_data
 
     class Meta:
