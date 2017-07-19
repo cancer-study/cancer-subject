75f136e252e71774c2989f42bd4622ddafa4dd04
aseokana <aseokana@bhp.org.bw>
2017-07-19 11:47:20 +0200

    removing unused code

--- cancer_subject/forms/subject_consent_form.py
+++ cancer_subject/forms/subject_consent_form.py
@@ -1,7 +1,5 @@
 from django import forms
 
-from edc_constants.choices import YES_NO
-
 from cancer_subject_validations.form_validators import (
     SubjectConsentFormValidation)
 
@@ -15,21 +13,6 @@ class SubjectConsentForm(forms.ModelForm):
         cleaned_data = SubjectConsentFormValidation(
             cleaned_data=cleaned_data).clean()
         return cleaned_data
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['guardian_name'].label = (
-            'Guardian\'s Last and first name (for patients with Abnormal mental status)')
-        self.fields['guardian_name'].help_text = (
-            'Required only if subject is unconscious or has an abnormal mental '
-            'status. Format is \'LASTNAME, FIRSTNAME\'. All uppercase separated '
-            'by a comma then followed by a space.')
-
-    store_genetic_samples = forms.ChoiceField(
-        label=('Does the subject agree that a portion of the blood sample '
-               'that is taken be stored for genetic analysis?'),
-        choices=YES_NO,
-        widget=forms.RadioSelect)
 
     class Meta:
         model = SubjectConsent
