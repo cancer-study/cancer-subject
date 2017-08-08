b771160fb1b317b614ac99eb668c4cda4dd287c1
Gofaone Magodi <magodign@magodign.local>
2017-08-08 13:09:48 +0200

    cancer update

--- cancer_subject/forms/subject_consent_form.py
+++ cancer_subject/forms/subject_consent_form.py
@@ -15,6 +15,36 @@ class SubjectConsentForm(forms.ModelForm):
             cleaned_data=cleaned_data).clean()
         return cleaned_data
 
+#     def validate_data_fields(self):
+#         screening_identifier = self.cleaned_data.get(
+#             'screening_identifier')
+#         first_name = self.cleaned_data.get('first_name')
+#         initials = self.cleaned_data.get('initials')
+#
+#         try:
+#             subject_eligibility = SubjectEligibility.objects.get(
+#                 screening_identifier=screening_identifier)
+#         except SubjectEligibility.DoesNotExist:
+#             raise forms.ValidationError(
+#                 f'Please complete \'{SubjectEligibility._meta.verbose_name}\' first.')
+#         else:
+#             if subject_eligibility.first_name != first_name:
+#                 raise forms.ValidationError({
+#                     'first_name': f'Does not match {SubjectEligibility._meta.verbose_name}'})
+#             if subject_eligibility.initials != initials:
+#                 raise forms.ValidationError({
+#                     'initials': f'Does not match {SubjectEligibility._meta.verbose_name}'})
+#
+#     study_site = forms.ChoiceField(
+#         label='Study site',
+#         choices=COMMUNITIES,
+#         help_text="",
+#         widget=forms.RadioSelect())
+#
+#     class Meta:
+#         model = SubjectConsent
+#         fields = '__all__'
+
     class Meta:
         model = SubjectConsent
         fields = '__all__'
