dee9aa076f01d22b88795d8a2626d72b90e52e80
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-10-27 10:57:13 +0200

    consents config

---
+++ cancer_subject/consents.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+import arrow
+from dateutil.tz import gettz
+from django.apps import apps as django_apps
+from edc_constants.constants import MALE, FEMALE
+
+from edc_consent.consent import Consent
+from edc_consent.site_consents import site_consents
+
+
+app_config = django_apps.get_app_config('edc_protocol')
+
+tzinfo = gettz('Africa/Gaborone')
+
+v1 = Consent(
+    'cancer_subject.subjectconsent',
+    version='1',
+    start=arrow.get(
+        datetime(2013, 12, 31, 0, 0, 0), tzinfo=tzinfo).to('UTC').datetime,
+    end=arrow.get(
+        datetime(2019, 12, 31, 23, 59, 59), tzinfo=tzinfo).to('UTC').datetime,
+    age_min=18,
+    age_is_adult=18,
+    age_max=64,
+    gender=[MALE, FEMALE])
+
+site_consents.register(v1)
