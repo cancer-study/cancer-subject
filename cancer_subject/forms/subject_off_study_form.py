e622a4e844ff48fd129b977e1b94b4f6a9cfd7f0
Kwasi <kdarkwa@bhp.org>
2017-11-28 09:18:42 +0200

    OffStudyModelForm  mixin fix

--- cancer_subject/forms/subject_off_study_form.py
+++ cancer_subject/forms/subject_off_study_form.py
@@ -1,11 +1,11 @@
-from edc_offstudy.modelform_mixins import OffstudyModelFormMixin
+from edc_offstudy.modelform_mixins import OffStudyFormMixin
 
 from ..models.subject_offstudy import SubjectOffstudy
 
 from .form_mixins import SubjectModelFormMixin
 
 
-class SubjectOffStudyForm (OffstudyModelFormMixin, SubjectModelFormMixin):
+class SubjectOffStudyForm (OffStudyFormMixin, SubjectModelFormMixin):
 
     class Meta:
         model = SubjectOffstudy
