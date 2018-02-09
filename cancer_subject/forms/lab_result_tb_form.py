b9f7640718c6a8a29e71771219aa284a530a204a
aseokana <aseokana@bhp.org.bw>
2018-02-09 11:55:25 +0200

    LabResultTb form

---
+++ cancer_subject/forms/lab_result_tb_form.py
@@ -0,0 +1,9 @@
+from ..models import LabResultTb
+from .form_mixins import SubjectModelFormMixin
+
+
+class LabResultTbForm(SubjectModelFormMixin):
+
+    class Meta:
+        model = LabResultTb
+        fields = '__all__'
