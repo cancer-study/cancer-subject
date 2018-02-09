076a01bf4d5d30c89cc83b5da1de7d06c81c83af
Itumeleng Mosweu <imosweuk@gmail.com>
2018-02-09 09:32:57 +0200

    remove unused help text model

--- cancer_subject/models/ae010.py
+++ cancer_subject/models/ae010.py
@@ -2,38 +2,33 @@ from django.db import models
 
 from .model_mixins import CrfModelMixin
 
-from ..choices.ae010 import (REPORT_TYPE_CHOICE,
-                             RELATIONSHIP_DESCRIPTION_CHOICE)
+from ..choices.ae010 import REPORT_TYPE_CHOICE, RELATIONSHIP_DESCRIPTION_CHOICE
 
 
 class Ae010 (CrfModelMixin):
 
     report_type = models.CharField(
-        verbose_name="1. Which type of report is this? ",
+        verbose_name="Which type of report is this? ",
         max_length=35,
         choices=REPORT_TYPE_CHOICE,
-        help_text="",
     )
 
     onset_date = models.DateTimeField(
-        verbose_name="2. Date of onset of event being reported here:",
+        verbose_name="Date of onset of event being reported here:",
         max_length=25,
-        help_text="",
     )
 
     event_grade = models.CharField(
-        verbose_name="4. =Grade of primary event (use grading scale "
+        verbose_name="Grade of primary event (use grading scale "
         "1-5, where 5=death) ",
         max_length=15,
-        help_text="",
     )
 
     relationship_description = models.CharField(
-        verbose_name="5. Please describe the relationship between "
+        verbose_name="Please describe the relationship between "
         "this adverse event and study activities:",
         max_length=65,
         choices=RELATIONSHIP_DESCRIPTION_CHOICE,
-        help_text="",
     )
 
     class Meta(CrfModelMixin.Meta):
