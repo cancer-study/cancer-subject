803a9372ae2ee3c4e99755e4b16963ca1e852a38
aseokana <aseokana@bhp.org.bw>
2018-02-12 08:30:16 +0200

    form validations

--- cancer_subject/forms/lab_result_tb_form.py
+++ cancer_subject/forms/lab_result_tb_form.py
@@ -1,8 +1,21 @@
+from django import forms
+
 from ..models import LabResultTb
 from .form_mixins import SubjectModelFormMixin
 
 
 class LabResultTbForm(SubjectModelFormMixin):
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data['tb_treatment'] == 'Yes, isoniazid preventative therapy (IPT)' and not cleaned_data['tb_treatment_start']:
+            raise forms.ValidationError(
+                'If participant is receiving any kind of treatment, when did this treatment begin')
+        if cleaned_data['tb_treatment'] == 'Yes, combination anti-tuberculosis treatment (ATT)' and not cleaned_data['tb_treatment_start']:
+            raise forms.ValidationError(
+                'If participant is receiving any kind of treatment, when did this treatment begin')
+        cleaned_data = super(LabResultTbForm, self).clean()
+        return cleaned_data
 
     class Meta:
         model = LabResultTb
