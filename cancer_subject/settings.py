7a74aedf2c026523c3fe2f38fbf353bafcc58802
aseokana <aseokana@bhp.org.bw>
2018-02-14 12:34:21 +0200

    adding edc_dashboard middleware

--- cancer_subject/settings.py
+++ cancer_subject/settings.py
@@ -83,7 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #    'edc_dashboard.middleware.DashboardMiddleware',
+    'edc_dashboard.middleware.DashboardMiddleware',
     'edc_subject_dashboard.middleware.DashboardMiddleware',
 ]
 
