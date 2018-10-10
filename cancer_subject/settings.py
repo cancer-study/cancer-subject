6e13d2421a9fb700f9d8f09a17484c8279eadba4
tumiemosweu <imosweuk@gmail.com>
2018-10-10 08:16:00 +0200

    update default db]

--- cancer_subject/settings.py
+++ cancer_subject/settings.py
@@ -117,20 +117,20 @@ WSGI_APPLICATION = 'cancer_subject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(ETC_DIR, 'mysql.conf'),
-        },
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': os.path.join(ETC_DIR, 'mysql.conf'),
+#         },
+#     },
+# }
 
 
 # Password validation
