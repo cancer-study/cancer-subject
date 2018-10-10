8ee14e1ee3ca958d40eb875fa2f1e2df1d23015b
tumiemosweu <imosweuk@gmail.com>
2018-10-10 12:42:01 +0200

    comment out tests

--- cancer_subject/tests/test_subject_rules.py
+++ cancer_subject/tests/test_subject_rules.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
-
-
-class TestSubjectRules(TestCase):
-
-    def setUp(self):
-        pass
+# from django.test import TestCase
+#
+#
+# class TestSubjectRules(TestCase):
+#
+#     def setUp(self):
+#         pass
