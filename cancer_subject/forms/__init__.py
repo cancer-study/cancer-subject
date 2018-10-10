a9397dba2c8e656a6cfc1705858d1d1c9f514e76
tumiemosweu <imosweuk@gmail.com>
2018-10-10 08:24:04 +0200

    remove appointment model

--- cancer_subject/forms/__init__.py
+++ cancer_subject/forms/__init__.py
@@ -2,7 +2,6 @@ from .activity_and_functioning import ActivityAndFunctioningForm
 from .ae010_form import Ae010Form
 from .af004_form import Af004Form
 from .af005_form import Af005Form
-from .appointment_form import AppointmentForm
 from .base_risk_assessment_alcohol_form import BaseRiskAssessmentAlcoholForm
 from .base_risk_assessment_cancer_form import BaseRiskAssessmentCancerForm
 from .base_risk_assessment_chemical_form import BaseRiskAssessmentChemicalForm
@@ -15,24 +14,24 @@ from .base_risk_assessment_mining_form import BaseRiskAssessmentMiningForm
 from .base_risk_assessment_smoking_form import BaseRiskAssessmentSmokingForm
 from .base_risk_assessment_sun_form import BaseRiskAssessmentSunForm
 from .baseline_hiv_history_form import BaselineHIVHistoryForm
+from .bhh_cd4_form import BHHCd4Form
 from .bhh_hiv_test_form import BHHHivTestForm
 from .bhh_who_illness_form import BHHWhoIllnessForm
-from .bhh_cd4_form import BHHCd4Form
 from .cancer_diagnoses_form import CancerDiagnosisForm
 from .enrollment_checklist_form import EnrollmentChecklistForm
-from .haart_record_form import HaartRecordForm
 from .haart_med_record_form import HaartMedRecordForm
+from .haart_record_form import HaartRecordForm
+from .lab_result_cd4_form import LabResultCd4Form
+from .lab_result_chemistry_form import LabResultChemistryForm
 from .lab_result_form import LabResultForm
 from .lab_result_haemotology_form import LabResultHaematologyForm
 from .lab_result_height_weight_form import LabResultHeightWeightForm
 from .lab_result_hiv_form import LabResultHivForm
 from .lab_result_tb_form import LabResultTbForm
 from .lab_result_viralload_form import LabResultViralloadForm
-from .lab_result_chemistry_form import LabResultChemistryForm
-from .lab_result_cd4_form import LabResultCd4Form
-from .oncology_treatment_record_form import OncologyTreatmentRecordForm
-from .oncology_treatment_plan_form import OncologyTreatmentPlanForm
 from .oncology_treatment_completed_form import OncologyTreatmentCompletedForm
+from .oncology_treatment_plan_form import OncologyTreatmentPlanForm
+from .oncology_treatment_record_form import OncologyTreatmentRecordForm
 from .otr_chemo_form import OTRChemoForm
 from .otr_radiation_form import OTRRadiationForm
 from .otr_surgical_form import OTRSurgicalForm
@@ -42,3 +41,6 @@ from .subject_locator_form import SubjectLocatorForm
 from .subject_off_study_form import SubjectOffStudyForm
 from .subject_visit_form import SubjectVisitForm
 from .symptoms_and_testing_form import SymptomsAndTestingForm
+
+
+#from .appointment_form import AppointmentForm
