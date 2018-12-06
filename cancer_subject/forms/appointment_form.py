35abbed86424a15424590314dd0a27fcaa2e5557
Leslie <leslie.kep@gmail.com>
2018-12-06 11:14:28 +0200

    override validator method

--- cancer_subject/forms/appointment_form.py
+++ cancer_subject/forms/appointment_form.py
@@ -1,15 +1,19 @@
 from django import forms
+from edc_appointment.form_validators import AppointmentFormValidator
 from edc_appointment.models.appointment import Appointment
 
 from ..choices import APPOINTMENT_REASON
 
 
-class AppointmentForm(forms.ModelForm):
+class AppointmentForm(AppointmentFormValidator, forms.ModelForm):
 
     appt_reason = forms.ChoiceField(
         label='Reason for appointment:',
         choices=APPOINTMENT_REASON,
         widget=forms.RadioSelect)
+
+    def validate_appt_reason(self):
+        pass
 
     class Meta:
         model = Appointment
