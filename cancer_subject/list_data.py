1b15fbf30a3fe1ca24d948dc824f3a38855ef78d
ckgathi <ckgathi@gmail.com>
2019-05-06 17:04:51 +0200

    update list data

--- cancer_subject/list_data.py
+++ cancer_subject/list_data.py
@@ -28,6 +28,33 @@ list_data = {
         (OTHER, 'Other, specify'),
         (NONE, 'None')
     ],
+    'cancer_prn.deathcauseinfo': [
+        ('No information will ever be available',
+         'No information will ever be available'),
+        ('Autopsy', 'Autopsy'),
+        ('Clinical record', 'Clinical record'),
+        ('Information from physician/nurse/other health care provider',
+         'Information from physician/nurse/other health care provider'),
+        ('Information from participant\u2019s relatives or friends',
+         'Information from participant\u2019s relatives or friends'),
+        ('Information requested, still pending',
+         'Information requested, still pending'),
+        ('Other, specify', 'Other, specify')
+    ],
+    'cancer_prn.causecategory': [
+        ('No information will ever be available',
+        'No information will ever be available'),
+        ('Cancer ', 'Cancer '),
+        ('HIV infection or HIV/AIDS-related diagnosis',
+        'HIV infection or HIV/AIDS-related diagnosis'),
+        ('Disease/injury unrelated to cancer or HIV',
+        'Disease/injury unrelated to cancer or HIV'),
+        ('Toxicity from cancer treatment (complications of chemotherapy, radiation, or surgery)',
+        'Toxicity from cancer treatment '),
+        ('Toxicity from HIV/AIDS treatment (HAART or treatment of HIV/AIDS-related diagnosis)',
+        'Toxicity from HIV/AIDS treatment'),
+        ('Other, specify', 'Other, specify')
+    ],
 }
 
 preload_data = PreloadData(
