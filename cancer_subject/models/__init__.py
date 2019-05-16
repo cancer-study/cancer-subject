ebf75757fa7ce3f5d768bf78a48997a5855381e9
ckgathi <ckgathi@gmail.com>
2019-05-16 06:01:08 +0200

    remove unused imports

--- cancer_subject/models/__init__.py
+++ cancer_subject/models/__init__.py
@@ -46,7 +46,6 @@ from .referral import Referral
 from .signals import subject_screening_on_post_save
 from .subject_consent import SubjectConsent
 from .subject_locator import SubjectLocator
-# from .subject_offstudy import SubjectOffstudy
 from .subject_requisition import SubjectRequisition
 from .subject_screening import SubjectScreening
 from .subject_visit import SubjectVisit
