aec2c027fc641014a4fa0d06ce48f8a32f388367
Leslie <leslie.kep@gmail.com>
2019-05-16 11:23:55 +0200

    override indirect contacts validator method

--- cancer_subject/forms/subject_locator_form.py
+++ cancer_subject/forms/subject_locator_form.py
@@ -1,8 +1,27 @@
 from django import forms
+from edc_constants.constants import YES, NO
 from edc_form_validators import FormValidatorMixin
-from edc_locator.forms.subject_locator_form_validator import SubjectLocatorFormValidator
+from edc_locator.forms import SubjectLocatorFormValidator as BaseSubjectLocatorFormValidator
 
 from ..models import SubjectLocator
+
+
+class SubjectLocatorFormValidator(BaseSubjectLocatorFormValidator):
+
+    def validate_indirect_contact(self):
+        self.required_if(
+            YES, field='may_contact_indirectly',
+            field_required='indirect_contact_name')
+
+        self.required_if(
+            YES, field='may_contact_indirectly',
+            field_required='indirect_contact_relation')
+
+        for field in ['indirect_contact_cell', 'indirect_contact_cell_alt',
+                      'indirect_contact_phone']:
+            self.not_required_if(
+                NO, field='may_contact_indirectly', field_required=field,
+                inverse=False)
 
 
 class SubjectLocatorForm(FormValidatorMixin, forms.ModelForm):
