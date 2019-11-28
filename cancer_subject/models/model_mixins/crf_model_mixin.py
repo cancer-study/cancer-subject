022e1e49be4318f5a813a2e1d43a6fc60000ce8d
Leslie <leslie.kep@gmail.com>
2019-11-28 18:19:53 +0200

    set offschedule compare datetimes, remove ununsed imports

--- cancer_subject/models/model_mixins/crf_model_mixin.py
+++ cancer_subject/models/model_mixins/crf_model_mixin.py
@@ -2,12 +2,9 @@ from django.db import models
 from django.db.models.deletion import PROTECT
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel, FormAsJSONModelMixin
-from edc_base.model_validators import datetime_not_future
 from edc_base.sites.site_model_mixin import SiteModelMixin
-from edc_base.utils import get_utcnow
 from edc_consent.model_mixins import RequiresConsentFieldsModelMixin
 from edc_metadata.model_mixins.updates import UpdatesCrfMetadataModelMixin
-from edc_offstudy.model_mixins import OffstudyCrfModelMixin
 from edc_reference.model_mixins import ReferenceModelMixin
 from edc_visit_schedule.model_mixins import SubjectScheduleCrfModelMixin
 from edc_visit_tracking.managers import (
@@ -15,6 +12,7 @@ from edc_visit_tracking.managers import (
 from edc_visit_tracking.model_mixins import (
     CrfModelMixin as VisitTrackingCrfModelMixin, PreviousVisitModelMixin)
 from edc_visit_tracking.model_mixins import CrfModelMixin as BaseCrfModelMixin
+
 
 from ..subject_visit import SubjectVisit
 
@@ -40,6 +38,7 @@ class CrfModelMixin(BaseCrfModelMixin, SubjectScheduleCrfModelMixin,
     """ Base model for all scheduled models
     """
 
+    offschedule_compare_dates_as_datetimes = True
     subject_visit = models.OneToOneField(SubjectVisit, on_delete=PROTECT)
 
     def natural_key(self):
